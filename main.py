f41422ae08c41a11b30a09316a4154a1b2d50d62
TwineTickler <seancarpentermusic@gmail.com>
2019-07-19 18:32:32 -0500

    finished __str__ and created road map for program design

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 class hockey_stick:
 
-    def __init__(self, id=False, make=False, model=False, hand=False,
+    def __init__(self, id=False, make=False, hand=False,
                 curve=False, flex=False, price=0):
         self.id = id
         self.make = make
-        self.model = model
         self.hand = hand
         self.curve = curve
         self.flex = flex
@@ -13,9 +12,36 @@ class hockey_stick:
     def __repr__(self):
         representation = {'id':self.id,
                         'make':self.make,
-                        'model':self.model,
                         'hand':self.hand,
                         'curve':self.curve,
                         'flex':self.flex,
                         'price':self.price}
         return(str(representation)) # hockey_stick() would not return a dict, so I had to cast to a string
+
+    def __str__(self):
+        strng = 'id: {}\nmake: {}\nhand: {}\ncurve: {}\nflex: {}\nprice: {}'.format(self.id, self.make, self.hand, self.curve, self.flex, self.price)
+        return(strng)
+
+    # def add_stick(self):
+    #     pass
+
+    # def get_details(self):
+    #     pass
+
+    # def purchase_stick(self):
+    #     pass
+
+    # Other things to do, first ask if they are a customer or manager
+    # 
+    # Manager can:
+    #   - Add new stick to inventory
+    #   - Remove stick from inventory
+    #   - View inventory (all details)
+    #   - Change price
+    #   - Quit
+    #
+    # Customer can:
+    #   - View inventory (not all details)
+    #   - View details of one stick
+    #   - Purchase Stick
+    #   - Quit
